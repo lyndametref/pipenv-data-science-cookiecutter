122b5c444075ce487852a637e7be6b57c6622f84
Lynda Metref <lynda.metref@gmail.com>
2020-11-25 20:12:04 +0100

    Correct PYTHONPATH prefix

--- {{ cookiecutter.project_slug }}/setup_env.py
+++ {{ cookiecutter.project_slug }}/setup_env.py
@@ -5,6 +5,6 @@ config_file = os.path.join(project_path, "config.ini")
 env_file = os.path.join(project_path, ".env")
 
 with open(env_file, 'w') as env:
-    env.write("PYTHONPATH=${PYTHONPATH}" + project_path + "\n")
+    env.write("PYTHONPATH=" + project_path + "\n")
     env.write("PROJECT_PATH=" + project_path + "\n")
     env.write("CONFIG_PATH=" + os.path.join(project_path, config_file) + "\n")
